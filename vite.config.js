881d300edce52d80c70502a36bb69e75ad83dadc
spotter <skytonic@gmail.com>
2026-04-11 01:55:18 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,35 @@ import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 import fs from 'fs'
 
-function copyHongdaeHtml() {
+// 지역별 HTML 파일을 dist로 복사하는 플러그인
+const REGION_HTML_FILES = [
+  'hongdae.html',
+  'seongsu.html',
+  'itaewon.html',
+  'euljiro.html',
+  'apgujeong.html',
+  'jamsil.html',
+  'magok.html',
+  'nowon.html',
+  'gangnam.html',
+  'cheonho.html',
+]
+
+function copyRegionHtmlFiles() {
   return {
-    name: 'copy-hongdae-html',
+    name: 'copy-region-html-files',
     closeBundle() {
-      fs.copyFileSync(
-        resolve(__dirname, 'hongdae.html'),
-        resolve(__dirname, 'dist/hongdae.html')
-      )
+      REGION_HTML_FILES.forEach(file => {
+        fs.copyFileSync(
+          resolve(__dirname, file),
+          resolve(__dirname, 'dist', file)
+        )
+      })
     }
   }
 }
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), copyHongdaeHtml()],
+  plugins: [react(), copyRegionHtmlFiles()],
 })
